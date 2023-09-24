d2fa3bd2db08cf54900d80154cd55ce52f1b5b69
khalid891 <141838720+khalid891@users.noreply.github.com>
2023-09-24 23:38:25 +0300

    Create account.router.js

---
+++ routers/account.router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const accountController = require('../controllers/account.controller');
+
+router.get('/login', accountController.getLogin);           
+router.post('/login', accountController.postLogin);         
+
+router.get('/logout', accountController.getLogout);         
+
+router.get('/profile', accountController.getProfile);      
+router.post('/profile', accountController.postProfile);     
+
+
+module.exports = router;
