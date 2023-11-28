f1b362522f46042324aabb5beec73c8206673301
Swalhamdan <sulaimanalhamdan640@gmail.com>
2023-11-28 20:51:14 +0300

    Token model as OOP

--- models/token.models.js
+++ models/token.models.js
@@ -1,17 +1,40 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require('./init');
 const { User } = require('./index');
+const UserClass = require('./user.model');
+var postmark = require("postmark");
+
+var client = new postmark.ServerClient(process.env.POSTMARK_SERVER_CLIENT_SECRET);
 
 class Token extends Model {
-    constructor(userObject) {
-        super();
+    async sendToken() {
+        let user = await UserClass.findByPk(this.UserId);
+        let uid = btoa(user.id);
 
-        let expireDate = new Date(new Date().getTime() + (30 * 60 * 1000));
-        
-        this.expiresAt = expireDate;
-        this.UserId = userObject.id;
-
-        return this;
+        let email = await client.sendEmail({
+            "From": "xpl-noreply@kuthbanhosting.com",
+            "To": user.email,
+            "Subject": `Verify Email Address for XLP`,
+            "HtmlBody": `<strong>Hello ${user.name},</strong><br>
+                        Click on the following link to activate your account:
+                        ${process.env.HOSTNAME}/account/activate/${uid}/${this.id}`,
+            "TextBody": `Hello ${user.name}, \n
+            Click on the following link to activate your account:
+            ${process.env.HOSTNAME}/account/activate/${uid}/${this.id}`,
+            "MessageStream": "outbound"
+        });        
+    }
+    setInvalidStatus() {
+        this.status = "invalid";
+        this.save();
+    }
+    consumeToken() {
+        this.status = 'consumed';
+        this.save()
+    }
+    setExpiredStatus() {
+        this.status = "expired";
+        this.save();
     }
 }
 Token.init({
@@ -21,6 +44,16 @@ Token.init({
         type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV4
     },
+    status: {
+        type: DataTypes.ENUM(
+            'valid', // Default value
+            'invalid', 
+            'consumed',
+            'expired' // If status was valid and time expired it is changed to
+        ),
+        defaultValue: 'valid',
+        allowNull: false
+    },
     expiresAt: {
         type: DataTypes.DATE,
         allowNull: false
@@ -29,5 +62,11 @@ Token.init({
     sequelize,
     include: [ User ]
 });
+
+Token.beforeValidate(function(token) { 
+    let expireDate = new Date(new Date().getTime() + (30 * 60 * 1000));
+        
+    token.expiresAt = expireDate;
+ });
 
 module.exports = Token;
