85a43724f7856ebcb73b3b91dbf1ffb13878e76f
Hmoudbader <144549753+Hmoudbader@users.noreply.github.com>
2023-12-12 19:26:57 +0300

    Update tokens.test.js

--- tests/tokens.test.js
+++ tests/tokens.test.js
@@ -1,50 +1,101 @@
-const chai = require('chai');
-const expect = chai.expect;
+const { Sequelize, DataTypes } = require('sequelize');
+const postmark = require('postmark');
 
-// Mocking the Token model and its rawAttributes
-const Token = {
-    rawAttributes: {
-        id: {
-            type: {
-                key: 'UUID'
-            }
-        },
-        token: {
-            type: {
-                key: 'UUID'
-            }
-        },
-        expiresAt: {
-            type: {
-                key: 'DATE'
-            }
-        }
-    },
-    create: async (data) => {
-        return data;
-    }
-};
+// Create a mock for Postmark's sendEmail method
+const mockSendEmail = jest.fn().mockResolvedValue(true);
 
-describe('Token Model', () => {
-    it('should have an id field of type UUID', () => {
-        expect(Token.rawAttributes.id.type.key).to.eql('UUID');
-    });
+// Mock the entire Postmark ServerClient
+jest.mock('postmark', () => ({
+  ServerClient: jest.fn().mockImplementation(() => ({
+    sendEmail: mockSendEmail,
+  })),
+}));
 
-    it('should have a token field of type UUID', () => {
-        expect(Token.rawAttributes.token.type.key).to.eql('UUID');
-    });
-
-    it('should have an expiresAt field of type DATE', () => {
-        expect(Token.rawAttributes.expiresAt.type.key).to.eql('DATE');
-    });
-
-    it('should create a new token', async () => {
-        const token = await Token.create({
-            id: '5321e241-4c0b-45a4-b8ec-9b52f283455e',
-            token: '1234e241-4c0b-45a4-b8ec-9b52f2834567',
-            expiresAt: new Date()
+class Token extends Sequelize.Model {
+    async sendToken() {
+        // Use the mocked Postmark client
+        const client = new postmark.ServerClient("POSTMARK_API_KEY"); // Replace with actual API key in real implementation
+        await client.sendEmail({
+            "From": "sender@example.com",
+            "To": "receiver@example.com",
+            "Subject": "Token",
+            "HtmlBody": "<strong>Your token</strong>",
+            "TextBody": "Your token",
         });
-        expect(token.id).to.eql('5321e241-4c0b-45a4-b8ec-9b52f283455e');
-        expect(token.token).to.eql('1234e241-4c0b-45a4-b8ec-9b52f2834567');
+    }
+
+    setInvalidStatus() {
+        this.status = 'invalid';
+        return this.save();
+    }
+
+    consumeToken() {
+        this.status = 'consumed';
+        return this.save();
+    }
+
+    setExpiredStatus() {
+        this.status = 'expired';
+        return this.save();
+    }
+}
+
+Token.init({
+    id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+    },
+    status: {
+        type: DataTypes.ENUM('valid', 'invalid', 'consumed', 'expired'),
+        defaultValue: 'valid',
+        allowNull: false
+    },
+    expiresAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: () => new Date(new Date().getTime() + (30 * 60 * 1000)) // 30 minutes from now
+    }
+}, { sequelize: new Sequelize('sqlite::memory:', { logging: false }), modelName: 'Token' });
+
+describe('Token Model Tests', () => {
+
+    beforeAll(async () => {
+        await Token.sequelize.sync();
+    });
+
+    test('initial status is valid', async () => {
+        const token = await Token.create({});
+        expect(token.status).toBe('valid');
+    });
+
+    test('sendToken sends an email', async () => {
+        const token = await Token.create({});
+        await token.sendToken();
+        // Check if Postmark's sendEmail mock function was called
+        expect(mockSendEmail).toHaveBeenCalled();
+    });
+
+    test('setInvalidStatus updates status to invalid', async () => {
+        const token = await Token.create({});
+        await token.setInvalidStatus();
+        expect(token.status).toBe('invalid');
+    });
+
+    test('consumeToken updates status to consumed', async () => {
+        const token = await Token.create({});
+        await token.consumeToken();
+        expect(token.status).toBe('consumed');
+    });
+
+    test('setExpiredStatus updates status to expired', async () => {
+        const token = await Token.create({});
+        await token.setExpiredStatus();
+        expect(token.status).toBe('expired');
+    });
+
+    afterAll(async () => {
+        await Token.sequelize.close();
     });
 });
